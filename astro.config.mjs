5e57d657b7350b291b7dda3f21afa0459bfc0629
Fred K. Schott <fkschott@gmail.com>
2024-03-14 09:30:48 -0700

    update to use generic node adapter

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,11 +2,13 @@ import { defineConfig } from 'astro/config';
 import db from "@astrojs/db";
 import react from "@astrojs/react";
 
-import vercel from "@astrojs/vercel/serverless";
+import node from "@astrojs/node";
 
 // https://astro.build/config
 export default defineConfig({
   integrations: [db(), react()],
   output: "server",
-  adapter: vercel()
+  adapter: node({
+    mode: "standalone"
+  })
 });
